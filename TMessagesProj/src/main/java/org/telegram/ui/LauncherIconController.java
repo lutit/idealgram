6110eb70e2b6c16c7d0ae2af149a433eb307792d
lutit <244531230+lutit@users.noreply.github.com>
2026-03-27 00:31:53 +0200

    deleted nagram app icons

--- TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
@@ -34,7 +34,6 @@ public class LauncherIconController {
     }
 
     public enum LauncherIcon {
-        DEFAULT("DefaultIcon", R.mipmap.ic_launcher_nagram, R.mipmap.icon_background_nagram, R.string.AppIconDefault),
         IDEALGRAM("IdealgramIcon", R.mipmap.ic_launcher_idealgram, R.mipmap.ic_launcher_idealgram_foreground, R.string.AppIconIdealgram),
         IDEALGRAM_BLUE("IdealgramIconBlue", R.mipmap.ic_launcher_idealgram_blue, R.mipmap.ic_launcher_idealgram_blue_foreground, R.string.AppIconIdealgramBlue),
         IDEALGRAM_RED("IdealgramIconRed", R.mipmap.ic_launcher_idealgram_red, R.mipmap.ic_launcher_idealgram_red_foreground, R.string.AppIconIdealgramRed),
@@ -43,14 +42,6 @@ public class LauncherIconController {
         IDEALGRAM_UZBEK("IdealgramIconUzbek", R.mipmap.ic_launcher_idealgram_uzbek, R.mipmap.ic_launcher_idealgram_uzbek_foreground, R.string.AppIconIdealgramUzbek),
         UZBEKGRAM("UzbekgramIcon", R.mipmap.ic_launcher_uzbekgram, R.mipmap.ic_launcher_uzbekgram_foreground, R.string.AppIconUzbekgram),
         UZBEKGRAM_LEGACY("UzbekgramLegacyIcon", R.mipmap.ic_launcher_uzbekgram_legacy, R.mipmap.ic_launcher_uzbekgram_legacy_foreground, R.string.AppIconUzbekgramLegacy),
-        GOOGLE("GoogleIcon", R.mipmap.icon_background_google, R.mipmap.icon_foreground_google, R.string.AppIconGoogle),
-        COLORFUL("ColorfulIcon", R.mipmap.icon_background_colorful, R.mipmap.icon_foreground_colorful, R.string.AppIconColorful),
-        DARKGREEN("DarkGreenIcon", R.mipmap.icon_background_darkgreen, R.mipmap.icon_foreground_darkgreen, R.string.AppIconDarkGreen),
-        NEON("NeonIcon", R.mipmap.icon_background_neon, R.mipmap.icon_foreground_neon, R.string.AppIconNeon),
-        NIELLO("NielloIcon", R.drawable.ic_launcher_nagram_round_niello_background, R.drawable.ic_launcher_nagram_round_niello_foreground, R.string.AppIconNiello),
-        BLUE("BlueIcon", R.color.nagram_block_round_background, R.drawable.ic_launcher_nagram_blue_foreground, R.string.AppIconBlue),
-        DARKBLUE("DarkBlueIcon", R.color.nagram_dark_blue_background, R.drawable.ic_launcher_nagram_dark_blue_foreground, R.string.AppIconDarkBlue),
-        BLURBLUE("BlurBlueIcon", R.drawable.ic_launcher_nagram_blur_blue_background, R.drawable.ic_launcher_nagram_blur_blue_foreground, R.string.AppIconBlurBlue),
         TELEGRAM("TelegramIcon", R.drawable.icon_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconTelegramOriginal),
         VINTAGE("VintageIcon", R.drawable.icon_6_background_sa, R.mipmap.icon_6_foreground_sa, R.string.AppIconVintage),
         AQUA("AquaIcon", R.drawable.icon_4_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconAqua),
@@ -86,7 +77,7 @@ public class LauncherIconController {
         }
 
         public boolean isNekoX() {
-            return this == DEFAULT;
+            return this == UZBEKGRAM_LEGACY;
         }
     }
 }
